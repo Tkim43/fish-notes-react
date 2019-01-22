1dfef12d89d01c893f14155d37a8a92330df54d8
Tiffany Kim <minikim9@hotmail.com>
2019-01-21 22:33:32 -0800

    completed project set up

---
+++ client/src/components/app.js
@@ -0,0 +1,14 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css';
+import React from 'react';
+
+const App = () => (
+    <div>
+        <div className="container">
+            <h1 className="center">Fish Notepad</h1>
+            <p className="center grey-text">Keep track of all your fish!</p>
+        </div>
+    </div>
+);
+
+export default App;

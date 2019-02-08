373961dbbe9db2b1d2bd39f1e78226701d26445d
Tiffany Kim <minikim9@hotmail.com>
2019-02-08 01:32:15 -0800

    in progress of completing onKey event for update mode

--- index.js
+++ index.js
@@ -127,6 +127,30 @@ app.patch('/update/:ID', async (req, res, next)=>{
     }
 }, errorHandling);
 
+app.patch('/api/delete/:ID', async (req, res, next)=>{
+    try {
+        const {ID} = req.params;
+
+        const query = 'DELETE FROM ?? WHERE ?? = ?';
+        const inserts = ['fish', 'ID', ID];
+
+        const sql = mysql.format(query, inserts);
+
+        const results = await db.query(sql);
+
+        res.send({
+            success: true,
+            results
+        });
+    } catch(err) {
+        req.status = 500;
+        req.error = 'Error posting category';
+
+        return next();
+    }
+}, errorHandling);
+
+
 
 //starts Express server on defined port
 app.listen(PORT, ()=>{
